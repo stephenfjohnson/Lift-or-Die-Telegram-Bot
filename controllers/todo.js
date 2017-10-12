8e4b2839780ffcd2e8c5d99f47c3e996b153de71
Stephen Johnson <mail@stephenfjohnson.com>
2017-10-11 22:06:13 -0700

    Basic WORKING todo App
    This is the save point for the todo app. Now converting it to a workout
    app!!

--- controllers/todo.js
+++ controllers/todo.js
@@ -4,6 +4,7 @@ const Telegram = require('telegram-node-bot');
 
 class TodoController extends Telegram.TelegramBaseController {
   addHandler($) {
+    $.getUserSession('I am alive 🤖 🤖 🤖')
     let todo = $.message.text.split(' ').slice(1).join(' '); // Removes the comand word from the message
 
     if (!todo) return $.sendMessage('Sorry please pass a todo item'); // Error message if command is not valid
@@ -23,13 +24,26 @@ class TodoController extends Telegram.TelegramBaseController {
     });
   }
 
+  checkHandler($) {
+    let index;
+    index = parseInt($.message.text.split(' ').slice(1)[0]);
+    if (isNaN(index)) return $.sendMessage('Sorry you didn\' pass a valid index');
+    $.getUserSession('todos').then(todos => {
+      if (index >= todos.length) return $.sendMessage('Sorry you didn\' pass a valid index');
+      todos.splice(index, 1);
+      $.setUserSession('todos', todos);
+      $.sendMessage('Checked TODO');
+    })
+  }
+
   get routes() {
     return {
       'addCommand': 'addHandler',
-      'getCommand': 'getHandler'
+      'getCommand': 'getHandler',
+      'checkCommand': 'checkHandler'
     };
   }
-  // My serialize function
+
   _serializeList(todoList) {
     let serialized = '*Your Todos:*\n';
     todoList.forEach((t, i) => {
@@ -38,14 +52,6 @@ class TodoController extends Telegram.TelegramBaseController {
     return serialized;
   }
 
-  // // His serialize function
-  // _serializeList(todoList) {
-  //   let serialized = '*Your Todos:*\n\n';
-  //   todoList.forEach((t, i) => {
-  //     serialized += `*${i}* - ${t}\n`;
-  //   });
-  //   return serialized;
-  //  }
 }
 
 module.exports = TodoController;
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 'use strict';
 
 const Telegram = require('telegram-node-bot'),
+  PersistentMemoryStorage = require('./adapters/PersistentMemoryStorage'),
+  storage = new PersistentMemoryStorage(
+    `${__dirname}/data/userStorage.json`,
+    `${__dirname}/data/chatStorage.json`
+  ),
   tg = new Telegram.Telegram('356500286:AAEaPNsrxCg4IaB8MDHfgdjg-mI8KLoItd0', {
-    workers: 1
+    workers: 1,
+    storage: storage
   });
 
 const  TodoController = require('./controllers/todo');
@@ -13,4 +19,14 @@ const todoCtrl = new TodoController();
 tg.router
   .when(new Telegram.TextCommand('/add', 'addCommand'), todoCtrl)
   .when(new Telegram.TextCommand('/get', 'getCommand'), todoCtrl)
+  .when(new Telegram.TextCommand('/check', 'checkCommand'), todoCtrl)
   .otherwise(new OtherwiseController());
+
+
+function exitHandler(exitCode) {
+  storage.flush();
+  process.exit(exitCode)
+}
+
+process.on('SIGINT', exitHandler.bind(null, 0));
+process.on('uncaughtException', exitHandler.bind(null, 1));
